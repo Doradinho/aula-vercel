597bf2e8b77e6724b26491a8c42183b846c2fc51
Doradinho <lawlietsora016@gmail.com>
2024-08-27 10:31:37 -0300

    mensagem

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-import express from "express";
+import express from 'express';
+import { montadoras, veiculos} from './dados.js';
 
 const app = express();
+app.use(express.json());
 
+app.get('/veiculo',(req,res)=>{
+    res.status(200).json("{'Server':'ok'}");
+})
 
+app.listen(3000,()=>{
+    let data = new Date();
+    console.log(`Sistema inicializado: \nInf:${data}`);
+    console.log('http://localhost:3000/');
+})
